1a5bdb51c26040103c5fd50adfa7d9b71ff48cab
Gaurip10 <thegauriparhad@gmail.com>
2021-08-29 20:40:01 +0530

    Added: Enter valid Email id

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -2,39 +2,66 @@ package com.bridgelabz;
 import java.util.regex.*;
 
 public class UserRegistration {
+    public static boolean isValidFirstUsername(String name)
+    {
+        String regex="^[Cap]\\w{5,29}[A-Za-z]$";
 
-        public static boolean isValidlastUsername(String name)
-        {
-            String regex="^[Cap]\\w{5,29}[A-Za-z]$";
+        Pattern p=Pattern.compile(regex);
 
-            Pattern p=Pattern.compile(regex);
-
-            if (name ==null){
-                return false;
-            }
-
-            //Pattern class contains matcher() method
-            //to find matching between given username
-            //and regular expression
-            Matcher m =p.matcher(name);
-
-            //Return if the username
-            return m.matches();
+        if (name ==null){
+            return false;
         }
-        public static void main(String[] args)
-        {
-            String str1="INDIA";
-            System.out.println(isValidFirstUsername(str1));
 
-            String str2="Cap4344";
-            System.out.println(isValidFirstUsername(str2));
+        //Pattern class contains matcher() method
+        //to find matching between given username
+        //and regular expression
+        Matcher m =p.matcher(name);
 
-            String str3="Cap2314";
-            System.out.println(isValidFirstUsername(str3));
+        //Return if the username
+        return m.matches();
+    }
+    public static boolean isValidlastUsername(String name)
+    {
+        String regex="^[Cap]\\w{5,29}[A-Za-z]$";
+
+        Pattern p=Pattern.compile(regex);
+
+        if (name ==null){
+            return false;
         }
+
+        //Pattern class contains matcher() method
+        //to find matching between given username
+        //and regular expression
+        Matcher m =p.matcher(name);
+
+        //Return if the username
+        return m.matches();
+    }
+
+    //Checking email Id:
+
+    public static boolean isValidEmail(String name) {
+
+        Pattern p = Pattern.compile("\\B[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+        Matcher m = p.matcher("thegauriparhad@gmail.com");
+
+    }
+
+
+
     public static void main(String[] args) {
-        //Checking for lastusername
+        String str1 = "INDIA";
+        System.out.println(isValidFirstUsername(str1));
 
+        String str2 = "Cap4344";
+        System.out.println(isValidFirstUsername(str2));
+
+        String str3 = "Cap2314";
+        System.out.println(isValidFirstUsername(str3));
+
+
+        //Checking for lastusername
         //Test Case:1
         String str1 = "Maharashtra";
         System.out.println(isValidlastUsername(str1));
@@ -46,6 +73,15 @@ public class UserRegistration {
         //Test Case:3
         String str3 = "Cap231xyz";
         System.out.println(isValidlastUsername(str3));
+
+        //checking mail-id:
+        String email = "thegauriparhad@gmail.com";
+        System.out.println(isValidEmail(email));
     }
 
+
+
 }
+
+
+    
